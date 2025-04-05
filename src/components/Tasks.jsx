fa314d09c31e22bcaffa958c28ad89f0793c1d45
Miguel Augusto Baricelli <baricellimiguel@gmail.com>
2025-04-04 23:17:58 -0300

    Update to App.jsx and Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,26 @@
- export function Tasks() {
-    return (
-        <h1>Foi</h1>
-    )
+function Tasks(props) {
+  console.log(props);
+
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow justify-center flex">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md text-left w-full ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            Ver
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
+export default Tasks;
